185e44fdd0a9380e5fd532c3c2a3265d2cc76d3e
sven <sjm324@cornell.edu>
2018-07-31 00:32:14 -0700

    apparently cd() is a problem too, i was trying to show the file path one

--- tests.py
+++ tests.py
@@ -1,8 +1,13 @@
 import os
 import platform
 import pytest
-import sys
 import textwrap
+
+
+# just to make things stick out in the logs online
+def color_print(msg):
+    for message in ["=" * 80, msg, "=" * 80]:
+        print("\033[36;1m{message}\033[0m".format(message=message))
 
 
 def make_it_big(prefix):
@@ -36,54 +41,99 @@ def full_absurd_path():
     return ABSURD_PATH
 
 
-# decorator to get sphinx and pytest to work together
-def setup_sphinx_details(method):
-    try:
-        if not os.path.isdir(full_absurd_path()):
-            os.makedirs(full_absurd_path())
-        print("==> Absurd directory path created.")
+# create testroot, testroot/conf.py, and testroot/index.rst so we can app.build
+def make_conf_and_index(testroot, extra_file=False):
+    if not os.path.isdir(testroot):
+        os.makedirs(testroot)
+    color_print("==> testroot directory path created.")
 
-        # create a conf.py
-        conf_py_path = os.path.join(full_absurd_path(), "conf.py")
-        with open(conf_py_path, "w") as conf_py:
-            conf_py.write(textwrap.dedent('''
-                # -*- coding: utf-8 -*-
-                project = 'super_long_names'
-                master_doc = 'index'
-                source_suffix = ['.rst']
+    # create a conf.py
+    conf_py_path = os.path.join(testroot, "conf.py")
+    with open(conf_py_path, "w") as conf_py:
+        conf_py.write(textwrap.dedent('''
+            # -*- coding: utf-8 -*-
+            project = 'super_long_names'
+            master_doc = 'index'
+            source_suffix = ['.rst']
+        '''))
+    color_print("==> The conf.py file has been written.")
+
+    if extra_file:
+        # scenario 2: create a file that has a name that will push it over the
+        # 260 character path limit.  In this case, sphinx will crash generating
+        # the doctree which should be solvable?
+        #
+        # tricky: we want the full path to be greater than 260, but not exceed
+        # filesystem limits on file length (usually around 255?)
+        curr_length = len(testroot)
+        desired_length = 262
+        # eeeeeeeeee......eeeeee.rst
+        extra_file_name = "e" * (desired_length - curr_length - 4) + ".rst"
+        with open(os.path.join(testroot, extra_file_name), "w") as ef:
+            ef.write(textwrap.dedent('''
+                Oh Look
+                =======
+
+                This is an extra file being generated!
             '''))
-        print("==> The conf.py file has been written.")
+        color_print("==> Extra file created: {0}".format(os.path.join(
+            testroot, extra_file_name
+        )))
 
-        # create an index.rst
-        index_rst_path = os.path.join(full_absurd_path(), "index.rst")
-        with open(index_rst_path, "w") as index_rst:
+    # create an index.rst
+    index_rst_path = os.path.join(testroot, "index.rst")
+    with open(index_rst_path, "w") as index_rst:
+        index_rst.write(textwrap.dedent('''
+            Super Long Names
+            ================
+
+            This is an index.rst!!!!
+        '''))
+
+        if extra_file:
+            # make sure this gets included in the build
             index_rst.write(textwrap.dedent('''
-                Super Long Names
-                ================
+                .. toctree::
+                   :maxdepth: 2
 
-                This project is able to be generated, but then will fail on
-                Windows because Sphinx is not checking for >= 260 characters
-                and inserting the magicial prefix.
-
-                It should fail during the doctree generation phase on AppVeyor.
-            '''))
-        print("==> The index.rst has been written.")
-
-        # mark the testing function so that the sphinx tests will be run
-        pytest.mark.sphinx(testroot=full_absurd_path())(method)
-
-        return method
-    except Exception as e:
-        sys.stderr.write("Could not perform test setup: {0}\n".format(e))
-        sys.exit(1)
+                   {extra_file_name}
+            '''.format(extra_file_name=extra_file_name)))
+    color_print("==> The index.rst has been written.")
 
 
-@setup_sphinx_details
-def test_it(app):
-    app.build()
-    index_html_path = os.path.join(
-        full_absurd_path(), "_build", "html", "index.html"
-    )
-    print("==> Generated index.html contents:")
+# decorator to get sphinx and pytest to work together
+def setup_sphinx_details(testroot, extra_file=False):
+    # see: https://stackoverflow.com/a/5929165/3814202
+
+    def real_decorator(function):
+        make_conf_and_index(testroot, extra_file=extra_file)
+
+        def wrapper(app):
+            function(app)
+
+        pytest.mark.sphinx(testroot=testroot)(wrapper)
+        return wrapper
+    return real_decorator
+
+
+# do not call unless you already did app.build!
+def cat_index_html(app):
+    index_html_path = os.path.join(app.outdir, "index.html")
+    color_print("==> Generated index.html contents:")
     with open(index_html_path) as index_html:
         print(index_html.read())
+
+
+@setup_sphinx_details(full_absurd_path())
+def test_testroot_too_long(app):
+    app.build()
+    cat_index_html(app)
+
+
+@setup_sphinx_details(
+    os.path.join(os.path.abspath(os.path.dirname(__file__)), "docs"),
+    extra_file=True
+)
+def test_rst_doc_too_long(app):
+    app.build()
+    cat_index_html(app)
